da09cd06a046ec480137cce745135623f55e3b99
Unknown <alejandro.jorge@opendeusto.es>
2019-03-22 11:39:25 +0100

    Errores

---
+++ Trabajo eclipse aplicacion Your team/src/COMUN/Constantes.java
@@ -0,0 +1,12 @@
+package COMUN;
+
+public class Constantes {
+	public static final String PROPIEDAD_clsEQUIPACION_COLOR1="color1";
+	 public static final String PROPIEDAD_clsEQUIPACION_COLOR2="color2";
+	 public static final String PROPIEDAD_clsEQUIPACION_DORSAL="dorsal";
+	 public static final String PROPIEDAD_clsEQUIPACION_PUBLICIDAD="publicidad";
+	 public static final String  PROPIEDAD_clsEQUIPACION_SERIGRAFIADO="serigrafiado";
+	 
+
+
+}
---
+++ Trabajo eclipse aplicacion Your team/src/COMUN/itfProperty.java
@@ -0,0 +1,7 @@
+package COMUN;
+
+public interface itfProperty {
+	Object getObjectProperty( String propiedad );
+	void setObjectProperty( String propiedad , Object valor );
+
+}
