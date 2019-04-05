be6c36e9af6d673f59fea72a0ccde1f80d4bc1ec
Aitor López <aitorlopez98@opendeusto.es>
2019-04-05 18:18:50 +0200

    Planificacion arreglada

--- Your Team/Trabajo eclipse aplicacion Your team/src/Main.java
+++ Your Team/Trabajo eclipse aplicacion Your team/src/Main.java
@@ -2,7 +2,7 @@ import LP.clsMenu;
 
 public class Main {
 	/**
-	 * A través del main se llama a todos los metodos anteriores y los imprimimos por pantalla y los guardamos en los arraylist
+	 * A trav?s del main se llama a todos los metodos anteriores y los imprimimos por pantalla y los guardamos en los arraylist
 	 * @param args
 	 */
 
