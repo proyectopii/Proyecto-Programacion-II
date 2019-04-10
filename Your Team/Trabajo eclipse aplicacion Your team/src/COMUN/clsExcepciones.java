b591c9aace4ea0905221c6bf1cd2202dbd1f1ab3
Unknown <alejandro.jorge@opendeusto.es>
2019-04-10 18:30:19 +0200

    excepciones runtime

--- Your Team/Trabajo eclipse aplicacion Your team/src/COMUN/clsExcepciones.java
+++ Your Team/Trabajo eclipse aplicacion Your team/src/COMUN/clsExcepciones.java
@@ -20,7 +20,10 @@ public class clsExcepciones extends RuntimeException {
 	public String getMessage() {
 		return mensaje;
 	}
-
+/**
+ *  Este caso es que saque un error para cuando haya un equipo repetido
+ * @param mensaje
+ */
 	public void clsEquipoRepetida(String mensaje) {
 		this.mensaje = mensaje;
 	}
@@ -28,7 +31,10 @@ public class clsExcepciones extends RuntimeException {
 	public String getMessage1() {
 		return mensaje;
 	}
-	
+	/**
+	 *  Este caso es que saque un error para cuando haya un escudo repetido
+	 * @param mensaje
+	 */
 	public void clsEscudoRepetida(String mensaje) {
 		this.mensaje = mensaje;
 	}
@@ -36,6 +42,10 @@ public class clsExcepciones extends RuntimeException {
 	public String getMessage2() {
 		return mensaje;
 	}
+	/**
+	 *  Este caso es que saque un error para cuando haya un intercambio repetido
+	 * @param mensaje
+	 */
 	
 	public void clsIntercambiooRepetida(String mensaje) {
 		this.mensaje = mensaje;
@@ -44,24 +54,36 @@ public class clsExcepciones extends RuntimeException {
 	public String getMessage3() {
 		return mensaje;
 	}
+	/**
+	 *  Este caso es que saque un error para cuando haya un jugador repetido
+	 * @param mensaje
+	 */
 	
-	public void clsJugadorRepetida(String mensaje) {
+	public void clsJugadorRepetido(String mensaje) {
 		this.mensaje = mensaje;
 	}
 
 	public String getMessage4() {
 		return mensaje;
 	}
+	/**
+	 *  Este caso es que saque un error para cuando haya un manager repetido
+	 * @param mensaje
+	 */
 	
-	public void clsManagerRepetida(String mensaje) {
+	public void clsManagerRepetido(String mensaje) {
 		this.mensaje = mensaje;
 	}
 
 	public String getMessage5() {
 		return mensaje;
 	}
+	/**
+	 *  Este caso es que saque un error para cuando haya un partido repetido
+	 * @param mensaje
+	 */
 	
-	public void clsPartidosRepetida(String mensaje) {
+	public void clsPartidosRepetido(String mensaje) {
 		this.mensaje = mensaje;
 	}
 
